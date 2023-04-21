2a7ad839baa123e7c974090b6b7792823e6b8513
Mia Tanner <mia@indygecko.com>
2023-04-21 13:48:02 -0700

    minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#show'
 
   get '/admin/:id' => 'admins#show'
-  get '/vote/:id' => 'vote#show'
+  get '/vote/:id' => 'votes#show'
   get '/new' => 'new#show'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
